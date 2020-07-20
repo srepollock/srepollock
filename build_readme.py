67feb1b2f0d0f738e31c23691c51f7efff728df6
Spencer Pollock <srepollock@users.noreply.github.com>
2020-07-20 09:59:46 -0700

    fix: (readme)
    Don't need the time

--- build_readme.py
+++ build_readme.py
@@ -25,7 +25,7 @@ def fetch_blog_entries():
             {
                 "title": entry["title"],
                 "url": entry["link"],
-                "published": entry["published"]
+                "published": entry["published"].split(r"[0-9]{4}").[1]
             }
             for entry in entries 
             ]
